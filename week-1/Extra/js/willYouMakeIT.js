23f365cfc16318949bc184f95b37bd41ff3b3d1a
ratel4150 <rratel76@gmail.com>
2022-03-27 23:57:03 +0200

    changes

---
+++ week-1/Extra/js/willYouMakeIT.js
@@ -0,0 +1,10 @@
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    if((distanceToPump/2)===mpg&&distanceToPump/mpg===fuelLeft){
+      return true
+    }else{
+      return false
+    }
+    // TODO
+  };
+
+  /* Falta en prolog NASM COBOL*/
