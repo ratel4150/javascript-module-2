75c5adaff782162b13ca53555c4351b7e83905ee
ratel4150 <rratel76@gmail.com>
2022-04-04 18:28:58 +0200

    exerciseM

--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,4 +1,64 @@
-function setAlarm() {}
+
+
+function setAlarm() {
+  let alarmSet = document.querySelector("#alarmSet").value;
+  let timeRemaining = document.querySelector("#timeRemaining");
+  
+  
+  intervalID1 = setInterval(function () {
+    if (alarmSet >= 0) {
+      //sonido
+      console.log(alarmSet);
+
+      if (alarmSet < 10) {
+        //unidades menores de 10
+        timeRemaining.textContent = "Time Remaining: 00:0" + alarmSet;
+      } else if (alarmSet < 60) {
+        //decenas menosres de 60
+        timeRemaining.textContent = "Time Remaining: 00:" + alarmSet;
+      } else if (alarmSet >= 60) {
+        //decenas myores o igual a 60
+        if (alarmSet / 60 < 10) {
+          if (alarmSet % 60 < 10) {
+            timeRemaining.textContent =
+              "Time Remaining: " +
+              "0" +
+              Math.round(alarmSet / 60) +
+              ":0" +
+              (alarmSet % 60);
+          } else {
+            timeRemaining.textContent =
+              "Time Remaining: " +
+              Math.round(alarmSet / 60) +
+              ":" +
+              (alarmSet % 60);
+          }
+        } else {
+          if (alarmSet % 60 < 10) {
+            timeRemaining.textContent =
+              "Time Remaining: " +
+              Math.round(alarmSet / 60) +
+              ":0" +
+              (alarmSet % 60);
+          } else {
+            timeRemaining.textContent =
+              "Time Remaining: " +
+              Math.round(alarmSet / 60) +
+              ":" +
+              (alarmSet % 60);
+          }
+        }
+      }
+      alarmSet--;
+    } else {
+      //sonido
+      timeRemaining.style.color="red"
+      playAlarm();
+    }
+  }, 1000);
+  
+}
+
 
 // DO NOT EDIT BELOW HERE
 
