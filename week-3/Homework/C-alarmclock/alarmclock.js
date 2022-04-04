85a0d38262a072e2b3d399eaecc489d7a90f38eb
ratel4150 <rratel76@gmail.com>
2022-04-04 19:16:05 +0200

    clock

--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,61 +1,77 @@
 
+let intervalID1
+let alarmSet
+let timeRemaining
 
-function setAlarm() {
-  let alarmSet = document.querySelector("#alarmSet").value;
-  let timeRemaining = document.querySelector("#timeRemaining");
+function prueba(){
   
-  
-  intervalID1 = setInterval(function () {
-    if (alarmSet >= 0) {
-      //sonido
-      console.log(alarmSet);
+  if (alarmSet >= 0) {
+    //sonido
+    console.log(alarmSet);
 
-      if (alarmSet < 10) {
-        //unidades menores de 10
-        timeRemaining.textContent = "Time Remaining: 00:0" + alarmSet;
-      } else if (alarmSet < 60) {
-        //decenas menosres de 60
-        timeRemaining.textContent = "Time Remaining: 00:" + alarmSet;
-      } else if (alarmSet >= 60) {
-        //decenas myores o igual a 60
-        if (alarmSet / 60 < 10) {
-          if (alarmSet % 60 < 10) {
-            timeRemaining.textContent =
-              "Time Remaining: " +
-              "0" +
-              Math.round(alarmSet / 60) +
-              ":0" +
-              (alarmSet % 60);
-          } else {
-            timeRemaining.textContent =
-              "Time Remaining: " +
-              Math.round(alarmSet / 60) +
-              ":" +
-              (alarmSet % 60);
-          }
+    if (alarmSet < 10) {
+      //unidades menores de 10
+      timeRemaining.textContent = "Time Remaining: 00:0" + alarmSet;
+    } else if (alarmSet < 60) {
+      //decenas menosres de 60
+      timeRemaining.textContent = "Time Remaining: 00:" + alarmSet;
+    } else if (alarmSet >= 60) {
+      //decenas myores o igual a 60
+      if (alarmSet / 60 < 10) {
+        if (alarmSet % 60 < 10) {
+          timeRemaining.textContent =
+            "Time Remaining: " +
+            "0" +
+            Math.round(alarmSet / 60) +
+            ":0" +
+            (alarmSet % 60);
         } else {
-          if (alarmSet % 60 < 10) {
-            timeRemaining.textContent =
-              "Time Remaining: " +
-              Math.round(alarmSet / 60) +
-              ":0" +
-              (alarmSet % 60);
-          } else {
-            timeRemaining.textContent =
-              "Time Remaining: " +
-              Math.round(alarmSet / 60) +
-              ":" +
-              (alarmSet % 60);
-          }
+          timeRemaining.textContent =
+            "Time Remaining: " +
+            Math.round(alarmSet / 60) +
+            ":" +
+            (alarmSet % 60);
+        }
+      } else {
+        if (alarmSet % 60 < 10) {
+          timeRemaining.textContent =
+            "Time Remaining: " +
+            Math.round(alarmSet / 60) +
+            ":0" +
+            (alarmSet % 60);
+        } else {
+          timeRemaining.textContent =
+            "Time Remaining: " +
+            Math.round(alarmSet / 60) +
+            ":" +
+            (alarmSet % 60);
         }
       }
-      alarmSet--;
-    } else {
-      //sonido
-      timeRemaining.style.color="red"
-      playAlarm();
     }
-  }, 1000);
+    alarmSet--;
+  } else {
+    //sonido
+    timeRemaining.style.color="red"
+    playAlarm();
+  }
+}
+  
+
+
+
+function setAlarm() {
+  if (!intervalID1) {
+    intervalID1 = setInterval(prueba, 1000,alarmSet = document.querySelector("#alarmSet").value, timeRemaining = document.querySelector("#timeRemaining"));
+  }
+
+
+}
+
+
+function pauseAlarm(){
+  clearInterval(intervalID1)
+  intervalID1=null
+  
   
 }
 
